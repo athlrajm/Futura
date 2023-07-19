274ab76d0bcf2132e24dd8f1984fbf1d888e15e2
athlrajm <athulrajm333@gmail.com>
2023-07-19 16:25:45 +0530

    update1

---
+++ JavaScript/Programs/LeftHalfPyramid.js
@@ -0,0 +1,16 @@
+function Pyramid()
+{
+    let i,j;
+    for(i=0;i<=5;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+        document.write("*"+ " ")
+        
+        
+    }
+    document.write("<br></br>")
+    
+}
+}
+Pyramid();
---
+++ JavaScript/Programs/Swap.js
@@ -0,0 +1,15 @@
+
+
+function Swap() {
+    let a=10,b=20,c;
+  
+    c=a+b;
+    a=c-a;
+    b=c-b;
+    document.write("after swapping"+" "+a +" "+b);
+  }
+  
+  
+  Swap();
+
+  
